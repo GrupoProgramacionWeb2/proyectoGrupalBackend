9cae37bc91524bcf1e01aac34ea3a3d81d2fb31f
joselinares170 <josedaniellinares135@gmail.com>
2022-11-23 13:47:42 -0500

    Empezando a agregar las relaciones a las tablas

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,6 +1,10 @@
 import { DataTypes } from 'sequelize';
-
 import sequelize from '../config/database.js';
+import Detalle_usuario from './detalle_usuario.js';
+import Reporte from './reporte.js';
+import Resena from './resena.js';
+import Orden from './orden.js';
+
 
 const Usuario = sequelize.define('usuario', {
 
@@ -22,6 +26,38 @@ const Usuario = sequelize.define('usuario', {
     contrasena: {
         type: DataTypes.STRING
     }
+})
+// desarrollo
+
+Usuario.hasMany(Detalle_usuario, {
+    sourceKey: 'usuario_id',
+    foreignKey: 'detalle_id'
+})
+
+Usuario.hasMany(Reporte, {
+    sourceKey: 'usuario_id',
+    foreignKey: 'reporte_id'
+})
+
+Usuario.hasMany(Resena, {
+    sourceKey: 'usuario_id',
+    foreignKey: 'resena_id'
+})
+
+Usuario.hasMany(Orden, {
+    sourceKey: 'usuario_id',
+    foreignKey: 'orden_id'
+})
+
+// muchos
+Professor.hasMany(Course,{ 
+    foreignKey: 'idProfessor',
+    sourceKey: 'id'
+})
+//pertenece
+Course.belongsTo(Professor,{
+    foreignKey: 'projectId',
+    targetId: 'id'
 })
 
 export default Usuario;
